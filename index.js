9a5b600cb010f303d0aa1aadd97c04d155a07397
Akib <akib3366@gmail.com>
2021-11-10 19:30:55 +0600

    Categories added

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-
 const app = require("./server");
 
-
 const port = process.env.PORT || 5000;
-
-
 
 // Initialize DB by runing the imported function
 require('./initDB')();
 
-
-
 //Tours Route
-// app.use('/categories', require('./Routes/Category.route'));
+app.use('/categories', require('./Routes/Category.route'));
 // app.use('/orders', require('./Routes/Order.route'));
-app.use('/products', require('./Routes/products/Product.route'));
+app.use('/products', require('./Routes/Product.route'));
 
 app.listen(port, () => {
     console.log(`Ranger Bikes server  listening at http://localhost:${port}`);
