aa96dfe60562a2a4cc439bbb2ed9c2a3e3d320b1
Akib <akib3366@gmail.com>
2021-11-16 22:53:20 +0600

    user route added

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ require('./initDB')();
 app.use('/categories', require('./Routes/Category.route'));
 app.use('/orders', require('./Routes/Order.route'));
 app.use('/products', require('./Routes/Product.route'));
+app.use('/users', require('./Routes/User.route'));
 
 app.listen(port, () => {
     console.log(`Ranger Bikes server  listening at http://localhost:${port}`);
